0dd5074839fae2fd6f9ea8f64599f8a3d282b566
Zev Hoffman <zevubu@gmail.com>
2019-09-18 19:57:33 -0700

    updated servers

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+ let tableArray = [
+     {
+        customerName:"Tom",
+        customerEmail:"Tom@tom.tom",
+        customerId:"Tom",
+        customerNumber:"555-555-5555",
+     },
+ ]
+
+ module.exports = tableArray;
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const Express = require("express");
+const express = require("express");
 
 const app = express();
-const PORT = 3035;
+const PORT = process.env.PORT || 3035;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+require("./routes/apiRoutes");
+require("./routes/htmlRoutes");
+
+app.listen(PORT, function(){
+    console.log(`App is listening to PORT: ${PORT}`);
+});
